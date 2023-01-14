e65d8c54e0dec0674ca6dfd52b43b223cc0e64ca
sophia <sophiendalo@gmail.com>
2023-01-14 15:23:31 +0300

    completed assignment

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Header({onDarkModeClick,isDarkMode}){
+    return(
+        
+        <div className={"App " + (isDarkMode ? "dark" : "light")}>
+      <header>
+        <h2>Shopster</h2>
+        <button onClick={onDarkModeClick}>
+          {isDarkMode ? "Dark" : "Light"} Mode
+        </button>
+      </header>
+      
+    </div>
+        
+    )
+}
+export default Header;
